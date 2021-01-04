82f3b5ec9aaeef5e62825ce019bfb0816b3ebc80
joshsrai <joshsrai@gmail.com>
2021-01-04 18:47:35 -0500

    commit tree 715d76ab5c75758e569a00febb3a1c75795ca945

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,30 @@
 class PostsController < ApplicationController
+  # helper_method :params
 
   def index
-    @posts = Post.all
-  end
+    # provide a list of authors to the view for the filter control
+    @authors = Author.all
+  
+    # filter the @posts list based on user input
+    if !params[:author].blank?
+      @posts = Post.by_author(params[:author])
+      # @posts = Post.where(author: params[:author])
+      # moved to parts of it to def self.by_author app/models/post.rb
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.from_today
+        # @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+        # moved to def self.from_today app/models/post.rb
+      else
+        @posts = Post.old_news
+        # @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+        # moved to def self.old_news app/models/post.rb
+      end
+    else
+      # if no filters are applied, show all posts
+      @posts = Post.all
+    end
+  end 
 
   def show
     @post = Post.find(params[:id])
